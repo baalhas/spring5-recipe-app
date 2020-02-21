1ba4fbcfecc873013d9662a675c9b14306a01b5f
Bahtiyar Ali ALHAS <bahtiyaralialhas@gmail.com>
2020-02-21 23:55:42 +0100

    Adding Enumerations and implementation.

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -18,7 +18,6 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-
     //todo add
     //private Difficulty difficulty;
 
@@ -27,6 +26,9 @@ public class Recipe {
 
     @Lob
     private Byte[] image;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
@@ -109,5 +111,21 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredientSet() {
+        return ingredientSet;
+    }
+
+    public void setIngredientSet(Set<Ingredient> ingredientSet) {
+        this.ingredientSet = ingredientSet;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
